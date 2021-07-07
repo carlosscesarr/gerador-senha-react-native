302a57f88ba8c51472cd957cc96ec877decbe0e7
Carlos <ce-sarr@hotmail.com>
2021-07-07 12:29:52 -0300

    Alteração de cores radio button, tamanho máximo de caracteres, adição de mais símbolos

--- App.js
+++ App.js
@@ -9,6 +9,7 @@
 import React, { useState } from 'react';
 import {
   SafeAreaView,
+  ScrollView,
   StyleSheet,
   View,
   Text,
@@ -22,9 +23,11 @@ import {
 import Slider from '@react-native-community/slider';
 import Clipboard from '@react-native-clipboard/clipboard';
 
-let letters = 'abcdefghijklmnopqrstuvwxyz'
-let numbers = '123456789'
-let simbols = '!@#$%&*()_-+=[]{}'
+let letters = 'abcdefghijklmnopqrstuvwxyz';
+let numbers = '0123456789';
+let simbols = '!#$%&()*+-:;<=>?@[]^_{}';
+let minCharacters = 1;
+let maxCharacters = 16;
 
 const App = () => {
   const [password, setPassword] = useState('');
@@ -33,36 +36,38 @@ const App = () => {
   const [isSortMaiusculo, setIsSortMaiusculo] = useState(true);
   const [isSortSimbol, setIsSortSimbol] = useState(true);
   const [isSortNumber, setIsSortNumber] = useState(true);
- 
-  function copyToClipboard(){
-    Clipboard.setString(password)
+
+  function copyToClipboard() {
+    Clipboard.setString(password);
   }
 
   function generatePassword() {
-    let minusculoLettersArray = letters.split('')
-    let maiusculoLettersArray = letters.toLocaleUpperCase().split('')
-    let numbersArray = numbers.split('')
-    let simbolsArray = simbols.split('')
+    let minusculoLettersArray = letters.split('');
+    let maiusculoLettersArray = letters.toLocaleUpperCase().split('');
+    let numbersArray = numbers.split('');
+    let simbolsArray = simbols.split('');
     let chacactersArray = [];
     if (isSortMinusculo) {
-      chacactersArray = [...chacactersArray, ...minusculoLettersArray]
+      chacactersArray = [...chacactersArray, ...minusculoLettersArray];
     }
     if (isSortMaiusculo) {
-      chacactersArray = [...chacactersArray, ...maiusculoLettersArray]
+      chacactersArray = [...chacactersArray, ...maiusculoLettersArray];
     }
     if (isSortNumber) {
-      chacactersArray = [...chacactersArray, ...numbersArray]
+      chacactersArray = [...chacactersArray, ...numbersArray];
     }
     if (isSortSimbol) {
-      chacactersArray = [...chacactersArray, ...simbolsArray]
+      chacactersArray = [...chacactersArray, ...simbolsArray];
     }
     let countCharset = chacactersArray.length;
-    let pass = ''
-    if (countCharset > 0) {
-      let chacactersArrayShufle = shuffle(chacactersArray);
-      for (let i = 1; i <= size; i++) {
-        pass += chacactersArrayShufle[Math.floor(Math.random() * countCharset)];
-      }
+    let pass = '';
+    if (!(countCharset > 0)) {
+      setPassword('');
+      return;
+    }
+    let chacactersArrayShufle = shuffle(chacactersArray);
+    for (let i = 1; i <= size; i++) {
+      pass += chacactersArrayShufle[Math.floor(Math.random() * countCharset)];
     }
     setPassword(pass);
   }
@@ -76,93 +81,102 @@ const App = () => {
     <>
       <StatusBar barStyle="dark-content" backgroundColor="#F3F3FF" />
       <SafeAreaView style={styles.container}>
-        <View>
-          <Image
-            style={styles.imagem}
-            source={require('./src/assets/security-protect-lock-01.png')}
-          />
-        </View>
-        <View>
-          <Text style={styles.countCharacter}>{size} Caractere(s)</Text>
-        </View>
-        <View style={styles.containerSlider}>
-          <Slider
-            style={styles.slider}
-            minimumValue={1}
-            maximumValue={15}
-            value={size}
-            onValueChange={(value) => setSize(parseInt(value))}
-            minimumTrackTintColor="#9651e9"
-            maximumTrackTintColor="#000000"
-            thumbTintColor="#9651e9"
-          />
-        </View>
-        <View style={styles.containerRaio}>
-          <Text style={styles.labelRadio}>Letras minúsculas</Text>
-          <Switch
-            trackColor={{ false: '#ddd', true: '#9a989c' }}
-            thumbColor={isSortMinusculo ? '#f0c330' : '#f4f3f4'}
-            ios_backgroundColor="#767577"
-            onValueChange={() => { setIsSortMinusculo(!isSortMinusculo) }}
-            value={isSortMinusculo}
-          />
-        </View>
-        <View style={styles.containerRaio}>
-          <Text style={styles.labelRadio}>Letras maiúsculas</Text>
-          <Switch
-            trackColor={{ false: '#ddd', true: '#9a989c' }}
-            thumbColor={isSortMaiusculo ? '#f0c330' : '#f4f3f4'}
-            ios_backgroundColor="#767577"
-            onValueChange={() => { setIsSortMaiusculo(!isSortMaiusculo) }}
-            value={isSortMaiusculo}
-          />
-        </View>
-        <View style={styles.containerRaio}>
-          <Text style={styles.labelRadio}>Símbolos</Text>
-          <Switch
-            trackColor={{ false: '#ddd', true: '#9a989c' }}
-            thumbColor={isSortSimbol ? '#f0c330' : '#f4f3f4'}
-            ios_backgroundColor="#3e3e3e"
-            onValueChange={() => { setIsSortSimbol(!isSortSimbol) }}
-            value={isSortSimbol}
-          />
-        </View>
-        <View style={styles.containerRaio}>
-          <Text style={styles.labelRadio}>Números</Text>
-          <Switch
-            trackColor={{ false: '#ddd', true: '#9a989c' }}
-            thumbColor={isSortNumber ? '#f0c330' : '#f4f3f4'}
-            ios_backgroundColor="#3e3e3e"
-            onValueChange={() => { setIsSortNumber(!isSortNumber) }}
-            value={isSortNumber}
-          />
-        </View>
+        <ScrollView style={{marginHorizontal: 20}}>
+          <View style={{width: '80%', marginTop: 25}}>
+            <Image
+              style={styles.imagem}
+              source={require('./src/assets/security-protect-lock-01.png')}
+            />
+          </View>
+          <View style={{width: '80%'}}>
+            <Text style={styles.countCharacter}>{size} Caracteres</Text>
+          </View>
+          <View style={styles.containerSlider}>
+            <Slider
+              style={styles.slider}
+              minimumValue={minCharacters}
+              maximumValue={maxCharacters}
+              value={size}
+              onValueChange={(value) => setSize(parseInt(value))}
+              minimumTrackTintColor="#9651e9"
+              maximumTrackTintColor="#000000"
+              thumbTintColor="#9651e9"
+            />
+          </View>
+          <View style={styles.containerRadio}>
+            <Text style={styles.labelRadio}>Letras minúsculas</Text>
+            <Switch
+              trackColor={{ false: '#ddd', true: '#f0cc00' }}
+              thumbColor={isSortMinusculo ? '#f0c330' : '#f4f3f4'}
+              ios_backgroundColor="#767577"
+              onValueChange={() => { setIsSortMinusculo(!isSortMinusculo) }}
+              value={isSortMinusculo}
+            />
+          </View>
+          <View style={styles.containerRadio}>
+            <Text style={styles.labelRadio}>Letras maiúsculas</Text>
+            <Switch
+              trackColor={{ false: '#ddd', true: '#f0cc00' }}
+              thumbColor={isSortMaiusculo ? '#f0c330' : '#f4f3f4'}
+              ios_backgroundColor="#767577"
+              onValueChange={() => { setIsSortMaiusculo(!isSortMaiusculo) }}
+              value={isSortMaiusculo}
+            />
+          </View>
+          <View style={styles.containerRadio}>
+            <Text style={styles.labelRadio}>Números</Text>
+            <Switch
+              trackColor={{ false: '#ddd', true: '#f0cc00' }}
+              thumbColor={isSortNumber ? '#f0c330' : '#f4f3f4'}
+              ios_backgroundColor="#3e3e3e"
+              onValueChange={() => { setIsSortNumber(!isSortNumber) }}
+              value={isSortNumber}
+            />
+          </View>
+          <View style={styles.containerRadio}>
+            
+              <Text style={styles.labelRadio}>Símbolos</Text>
+              <Switch
+                trackColor={{ false: '#ddd', true: '#f0cc00' }}
+                thumbColor={isSortSimbol ? '#f0c330' : '#f4f3f4'}
+                ios_backgroundColor="#3e3e3e"
+                onValueChange={() => { setIsSortSimbol(!isSortSimbol) }}
+                value={isSortSimbol}
+              />
+          </View>
 
-        {password !== '' && (
-          <TouchableHighlight
-            activeOpacity={1}
-            underlayColor="#FFF"
-            onPress={copyToClipboard}
-            style={styles.containerPassword}
-          >
-            <View style={{justifyContent: 'space-between', flexDirection: 'row', alignItems: 'center'}}>
-              <View style={{flex: 1}}>
-                <Text style={styles.textPassword}>{password}</Text>
+          {password !== '' && (
+            <TouchableHighlight
+              activeOpacity={1}
+              underlayColor="#FFF"
+              onPress={copyToClipboard}
+              style={styles.containerPassword}>
+              <View
+                style={{
+                  justifyContent: 'space-between',
+                  flexDirection: 'row',
+                  alignItems: 'center',
+                }}>
+                <View style={{ flex: 1 }}>
+                  <Text style={styles.textPassword}>{password}</Text>
+                </View>
+                <TouchableOpacity>
+                  <Image
+                    style={styles.copyImagePassword}
+                    source={require('./src/assets/copy.png')}
+                  />
+                </TouchableOpacity>
               </View>
-              <TouchableOpacity>
-                <Image
-                  style={styles.copyImagePassword}
-                  source={require('./src/assets/copy.png')}
-                />
-              </TouchableOpacity>
-            </View>
-          </TouchableHighlight>
-        )}
-        <View style={styles.containerSubmit}>
-          <TouchableOpacity onPress={generatePassword} style={styles.btnSubmit}>
-            <Text style={styles.textBtnSubmit}>Gerar</Text>
-          </TouchableOpacity>
-        </View>
+            </TouchableHighlight>
+          )}
+          <View style={styles.containerSubmit}>
+            <TouchableOpacity
+              onPress={generatePassword}
+              style={styles.btnSubmit}>
+              <Text style={styles.textBtnSubmit}>Gerar</Text>
+            </TouchableOpacity>
+          </View>
+        </ScrollView>
       </SafeAreaView>
     </>
   );
@@ -173,23 +187,26 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: "#F3F3FF",
+    backgroundColor: '#F3F3FF',
   },
   imagem: {
     width: 200,
     height: 200,
+    alignSelf: 'center',
   },
   countCharacter: {
     fontSize: 22,
     fontWeight: 'bold',
-    color: '#3e3e3e'
+    textAlign: 'center',
+    color: '#3e3e3e',
   },
   containerSlider: {
     marginTop: 14,
     marginBottom: 14,
     backgroundColor: '#FFF',
     borderRadius: 7,
-    width: '80%'
+    justifyContent: 'center',
+    width: '80%',
   },
   slider: {
     height: 50,
@@ -199,18 +216,18 @@ const styles = StyleSheet.create({
     height: 50,
     backgroundColor: '#9651e9',
     borderRadius: 7,
-    marginTop: 4,
+    marginTop: 10,
   },
   btnSubmit: {
     justifyContent: 'center',
     alignItems: 'center',
     width: '100%',
-    height: '100%'
+    height: '100%',
   },
   textBtnSubmit: {
     fontSize: 20,
     color: '#FFF',
-    fontWeight: 'bold'
+    fontWeight: 'bold',
   },
   containerPassword: {
     marginTop: 14,
@@ -220,8 +237,7 @@ const styles = StyleSheet.create({
     width: '80%',
     height: 50,
     justifyContent: 'center',
-    paddingHorizontal: 6
-
+    paddingHorizontal: 6,
   },
   copyImagePassword: {
     width: 24,
@@ -231,17 +247,17 @@ const styles = StyleSheet.create({
     fontSize: 22,
     alignSelf: 'center',
   },
-  containerRaio: {
+  containerRadio: {
     width: '80%',
     flexDirection: 'row',
     justifyContent: 'space-between',
-    marginVertical: 4
+    marginVertical: 4,
   },
   labelRadio: {
     fontSize: 17,
     fontWeight: 'bold',
-    color: '#3e3e3e'
-  }
+    color: '#3e3e3e',
+  },
 });
 
 export default App;
